8a44595181e092f7c0eb96bd0610b729c66b330f
gordon_west@CLOUD-DevOps-ENGINEER <gordonwest08@gmail.com>
2026-03-21 00:49:52 +0300

    added oidc to authenticate with AWS

--- infra/modules/iam/main.tf
+++ infra/modules/iam/main.tf
@@ -79,3 +79,24 @@ resource "aws_iam_role_policy_attachment" "ecs_task_attach" {
   role       = aws_iam_role.ecs_task_role.name
   policy_arn = aws_iam_policy.ecs_secrets_policy.arn
 }
+
+resource "aws_iam_role" "github_actions" {
+  name = "github-actions-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Federated = aws_iam_openid_connect_provider.github.arn
+      }
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Condition = {
+        StringLike = {
+          "token.actions.githubusercontent.com:sub" =
+          "repo:Gordonwest08/containerized-cloud-app-platform-with-CI-CD:*"
+        }
+      }
+    }]
+  })
+}
