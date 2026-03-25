2bc84c6bd454e827c349fefe47310c2929bdf2fa
gordon_west@CLOUD-DevOps-ENGINEER <gordonwest08@gmail.com>
2026-03-25 14:00:48 +0300

    added a production workflow file

---
+++ infra/environments/prod/main.tf
@@ -0,0 +1,25 @@
+module "vpc" {
+  source          = "../../modules/vpc"
+  name            = "prod-vpc"
+  cidr_block      = "10.1.0.0/16"
+  public_subnets  = ["10.1.1.0/24", "10.1.2.0/24"]
+  private_subnets = ["10.1.11.0/24", "10.1.12.0/24"]
+  azs             = ["us-east-1a", "us-east-1b"]
+}
+
+module "ecs" {
+  source                 = "../../modules/ecs"
+  cluster_name           = "prod-cluster"
+  env                    = "prod"
+  vpc_id                 = module.vpc.vpc_id
+  subnets                = module.vpc.private_subnets
+  public_subnets         = module.vpc.public_subnets
+  region                 = var.region
+
+  frontend_image         = var.frontend_image
+  db_image               = var.db_image
+
+  execution_role_arn     = data.terraform_remote_state.shared_iam.outputs.execution_role_arn
+  task_role_arn          = data.terraform_remote_state.shared_iam.outputs.ecs_task_role_arn
+  db_password_secret_arn = data.terraform_remote_state.shared_iam.outputs.db_password_secret_arn
+}
